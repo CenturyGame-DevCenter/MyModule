269edf1e53a741ae26e874d8dc38c3756d32bdb1
liuyunfei2020 <yunfei.liu@centurygame.com>
2020-05-27 18:42:53 +0800

    add my module

---
+++ Assets/MyModule/Scripts/Editor/MyModuleEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using MyModule;
+
+namespace MyModule.Editor
+{
+    public class MyModuleEditor
+    {
+        [MenuItem("MyModuleTool/Test")]
+        public static void MyModuleTool()
+        {
+            MyModuleTest t = new MyModuleTest();
+            t.Start();
+            t = null;
+        }
+    }
+}
