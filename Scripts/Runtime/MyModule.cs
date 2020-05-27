9fdee7b563f751114fc75bfb7b15d28c53a79e70
liuyunfei2020 <yunfei.liu@centurygame.com>
2020-05-27 18:42:53 +0800

    add my module

---
+++ Scripts/Runtime/MyModule.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace MyModule
+{
+    public class MyModuleTest
+    {
+        public void Start()
+        {
+            Debug.Log("Start MyModule");
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
